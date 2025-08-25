c9ce24013e52b22b391692d9f5292097824cbd04
Josep M Sobrepere <jm.sobrepere@gmail.com>
2025-08-25 09:00:41 +0800

    hack: make it compile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { textToMetadata } from "./textToMetadata";
-import { V15 } from "@polkadot-api/substrate-bindings";
 import {
   CodecComponentType,
   CodecComponentValue,
